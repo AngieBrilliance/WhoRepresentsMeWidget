37dd40fe5bacdaf2a3a539cb6463c894f488d27e
aliyarahman <aliya.rahman@yahoo.com>
2014-08-11 16:42:38 -0400

    Added OpenStates API calls and result views in pure JSON

--- WhoYouReppin/WhoRepresentsMe/views.py
+++ WhoYouReppin/WhoRepresentsMe/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 import requests
 from forms import EnterAddressForm
-from settings_hidden import GOOGLE_GEOCODE_API_KEY
+from settings_hidden import GOOGLE_GEOCODE_API_KEY, SUNLIGHT_API_KEY
 
 def index(request):
 	form = EnterAddressForm()
@@ -19,10 +19,16 @@ def showLegislators(request, address):
 	results = r.json()		# Save your answer to results
 	lat = results['results'][0]['geometry']['location']['lat']
 	lng = results['results'][0]['geometry']['location']['lng']
+
+	url = "http://openstates.org/api/v1//legislators/geo/?lat="+str(lat)+"&long="+str(lng)+"&apikey="+SUNLIGHT_API_KEY
+	r = requests.get(url)
+	legislators = r.json() 
+
 	form = EnterAddressForm()
+
 	if request.method == 'POST':
 		form = EnterAddressForm(request.POST)
 	if form.is_valid():
 		address = form.cleaned_data['address']
 		return HttpResponseRedirect('/WhoRepresentsMe/showLegislators/%s' % (address))
-	return render(request, 'results.html', {'lat':lat, 'lng':lng, 'form':form})
+	return render(request, 'results.html', {'lat':lat, 'lng':lng, 'form':form, 'legislators':legislators})
