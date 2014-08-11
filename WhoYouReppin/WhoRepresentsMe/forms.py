1cea07761c5270b7d24298acff1ed885f5424a8d
aliyarahman <aliya.rahman@yahoo.com>
2014-08-11 18:21:48 -0400

    Updated results view

--- WhoYouReppin/WhoRepresentsMe/forms.py
+++ WhoYouReppin/WhoRepresentsMe/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class EnterAddressForm(forms.Form):
-	address = forms.CharField(label="address", max_length=255, required=True)
+	address = forms.CharField(label="address", max_length=255, required=True,widget=forms.TextInput(attrs={"placeholder": "What's your address?"}))
